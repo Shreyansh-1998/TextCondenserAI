bf20c7c21baa5f474f9fe9643c47693a61a4d04f
Shreyansh-1998 <shreyanshsing1998@gmail.com>
2024-04-18 17:53:39 +0200

    Data Validation added

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.logging import logger
-
+from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 STAGE_NAME ="Data Ingestion Stage"
 
 try:
     logger.info(f">>>>Starting {STAGE_NAME} <<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>> Completed {STAGE_NAME} <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME ="Data Validation Stage"
+
+try:
+    logger.info(f">>>>Starting {STAGE_NAME} <<<<")
+    data_validation = DataValidationPipeline()
+    data_validation.main()
     logger.info(f">>>> Completed {STAGE_NAME} <<<<")
 except Exception as e:
     logger.exception(e)
--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -9,10 +9,11 @@ class DataIngestionTrainingPipeline:
         pass
 
     def main(self):
-      
         config_manager = ConfigurationManager()
         data_ingestion_config = config_manager.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+    
+      
         
