fe72dc81c03928b32ce8a9cbb6ccc71f9e02e3a7
Shreyansh-1998 <shreyanshsing1998@gmail.com>
2024-04-20 14:43:43 +0200

    Model Trainer and Model Evalution Added

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTra
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationPipeline
 from src.textSummarizer.pipeline.stage_03_data_transformation import DataTransformationPipeline
+from src.textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerPipeline
+from src.textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 STAGE_NAME ="Data Ingestion Stage"
 
@@ -36,3 +38,25 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME ="Model Training Stage"
+
+try:
+    logger.info(f">>>>Starting {STAGE_NAME} <<<<")
+    model_training = ModelTrainerPipeline()
+    model_training.main()
+    logger.info(f">>>> Completed {STAGE_NAME} <<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evaluation = ModelEvaluationTrainingPipeline()
+   model_evaluation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/textSummarizer/components/model_trainer.py
@@ -0,0 +1,55 @@
+from transformers import TrainingArguments,Trainer
+from transformers import DataCollatorForSeq2Seq
+from transformers import AutoModelForSeq2SeqLM,AutoTokenizer
+from datasets import load_dataset,load_from_disk
+import torch
+from src.textSummarizer.config import ModelTrainerConfig
+import os
+
+
+
+
+class ModelTrainer:
+
+    def __init__(self,config:ModelTrainerConfig):
+       self.config=config
+    
+
+    def train(self):
+        device="cuda" if torch.cuda.is_available() else "cpu"
+        print(f"device:{device}")
+        tokenizer=AutoTokenizer.from_pretrained(self.config.model_ckpt)
+        model_pegasus=AutoModelForSeq2SeqLM.from_pretrained(self.config.model_ckpt).to(device)
+        seq2seq_data_collator=DataCollatorForSeq2Seq(tokenizer, model=model_pegasus)#for batch processing
+
+        #load dataset
+        dataset_samsum_pt=load_from_disk(self.config.data_path)
+
+        trainer_args=TrainingArguments(
+            output_dir= self.config.root_dir,
+            num_train_epochs= self.config.num_train_epochs,
+            per_device_train_batch_size= self.config.per_device_train_batch_size,
+            warmup_steps= self.config.warmup_steps,
+            weight_decay= self.config.weight_decay,
+            logging_steps= self.config.logging_steps,
+            evaluation_strategy= self.config.evaluation_strategy,
+            eval_steps= self.config.eval_steps,
+            save_steps= 1e6,
+            gradient_accumulation_steps= self.config.gradient_accumulation_steps
+        )
+
+        trainer=Trainer(
+            model=model_pegasus,
+            data_collator=seq2seq_data_collator,
+            tokenizer=tokenizer,
+            args=trainer_args,
+            train_dataset=dataset_samsum_pt["test"],
+            eval_dataset=dataset_samsum_pt["validation"]
+        )
+
+        trainer.train()
+
+        ##save model
+        model_pegasus.save_pretrained(os.path.join(self.comfig.root_dir,"pegasus-samsum-model"))
+        ##save tokenizer
+        tokenizer.save_pretrained(os.path.join(self.config.root_dir,"tokenizer"))
